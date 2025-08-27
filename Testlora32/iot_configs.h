c09c6537bde4d272d4e30ddcdee7f01c6be8dedb
Breayann <breayanortiz@gmail.com>
2025-08-26 21:45:44 -0500

    First commit

---
+++ Testlora32/iot_configs.h
@@ -0,0 +1,15 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+// Wifi
+#define IOT_CONFIG_WIFI_SSID "JCAiPhone"
+#define IOT_CONFIG_WIFI_PASSWORD "puco12345"
+
+// Azure IoT
+#define IOT_CONFIG_IOTHUB_FQDN "ingenieriaiothub2.azure-devices.net"
+#define IOT_CONFIG_DEVICE_ID "JuanAcosta"
+#define IOT_CONFIG_DEVICE_KEY "A2C/QXdzR/Vc1/a2hLEELDgHk/2Ced48HAIoTEzlHh0="
+
+
+// Publish 1 message every 2 seconds
+#define TELEMETRY_FREQUENCY_MILLISECS 20000
